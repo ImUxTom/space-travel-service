985d0bae119944359aa15058fd6dec055328d4dd
Tom <thomas.h.brown@icloud.com>
2024-05-02 16:12:46 +0100

    Added destination.html name.html address.html check.html plus routes to routes.js

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,15 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post('/new/destination', (req, res) => {
+    res.redirect('/new/name')
+})
+
+router.post('/new/name', (req, res) => {
+    res.redirect('/new/address')
+})
+
+router.post('/new/address', (req, res) => {
+    res.redirect('/new/check')
+})
