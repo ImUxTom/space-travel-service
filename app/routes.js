fd768a624319699a8c445f228d831a1a0a7f2d1a
Tom <thomas.h.brown@icloud.com>
2024-05-03 13:58:04 +0100

    Fixed text in success panel to render new.destination

--- app/routes.js
+++ app/routes.js
@@ -8,23 +8,35 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
+
 router.post('/new/destination', (req, res) => {
-
-    if (req.body.new.destination ==  'Neither - I do not want to go to the Moon or Mars') {
-        res.redirect('/new/cannot-travel')    
-    }
-    else   
+    if(req.body.new.destination == 'Neither - I do not want to go to the Moon or Mars') {
+      res.redirect('/new/cannot-travel')
+    } else {
+      if(req.query.returnUrl) {
+        res.redirect(`${req.query.returnUrl}`)
+      } else {
         res.redirect('/new/name')
-    
-})
+      }
+    }
+  })
 
-router.post('/new/name', (req, res) => {
-    res.redirect('/new/address')
-})
 
-router.post('/new/address', (req, res) => {
-    res.redirect('/new/check')
-})
+  router.post('/new/name', (req, res) => {
+    if(req.query.returnUrl) {
+      res.redirect(`${req.query.returnUrl}`)
+    } else {
+      res.redirect('/new/address')
+    }
+  })
+
+  router.post('/new/address', (req, res) => {
+    if(req.query.returnUrl) {
+      res.redirect(`${req.query.returnUrl}`)
+    } else {
+      res.redirect('/new/check')
+    }
+  })
 
 
 router.post('/new/check', (req, res) => {
